9982759c60255828d2bb83def90c2c0877d41a3c
griger <gustavo.rg92@gmail.com>
2016-04-23 01:12:28 +0200

    nada

--- AGG.py
+++ AGG.py
@@ -2,7 +2,7 @@ import numpy as np
 from knnLooGPU import *
 
 def cross(first_parent, second_parent, a,b):
-    second_parent_gens
+    first_parent[a:b+1], second_parent[a:b+1] = np.copy(second_parent[a:b+1]), np.copy(first_parent[a:b+1])
 
 def mutate(s, gen_idx):
     s[gen_idx] = not s[gen_idx]
