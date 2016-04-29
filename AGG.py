fed3b9ede3f09f3ae4581c90eeb52d9e3ce7db68
griger <gustavo.rg92@gmail.com>
2016-04-29 19:17:13 +0200

    Operador de cruce alternativo

--- AGG.py
+++ AGG.py
@@ -1,8 +1,18 @@
 import numpy as np
 from knnLooGPU import *
 
-def cross(first_parent, second_parent, a,b):
-    first_parent[a:b+1], second_parent[a:b+1] = np.copy(second_parent[a:b+1]), np.copy(first_parent[a:b+1])
+def cross(first_parent, second_parent, first_son, second_son):
+    not_equals_idx = first_parent ^ second_parent
+    equals_idx = np.logical_not(not_equals_idx)
+
+    #copy identical genes into children
+    first_son = first_parent & equals_idx
+    second_son = first_parent & equals_idx
+
+    #random different genes selection
+    not_equals_size = sum(not_equals_idx)
+    first_son[not_equals_idx] = np.random.choice([True, False], not_equals_size)
+    second_son[not_equals_idx] = np.logical_not(first_son[not_equals_idx])
 
 def mutate(s, gen_idx):
     s[gen_idx] = not s[gen_idx]
