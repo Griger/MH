8f04b90c9ce7fac726faac1859f04fdb7c370fd3
griger <gustavo.rg92@gmail.com>
2016-04-30 12:18:37 +0200

    AGG initialize + selection + cross (Not tested and Not checked

--- AGG.py
+++ AGG.py
@@ -18,8 +18,33 @@ def mutate(s, gen_idx):
     s[gen_idx] = not s[gen_idx]
 
 def AGG(train_data, train_labels, knnGPU):
-    #poblacion inicial
     n = len(train_data[0])
-    p_size = 30 #poblation size
+    p_size = 30 #population size
 
-    P0 = np.random.choice([True, False], n*p_size).reshape((p_size, n)) #initial poblation
+    size_chromosome_string = str(n) + 'bool'
+    datatype = np.dtype( [('chromosome',size_chromosome_string), ('score',np.float32)] )
+
+    parent = np.zeros(p_size, dtype=datatype)
+    parent["chromosome"] = np.random.choice([True,False], (p_size, n)) #random initial population
+
+    for individual in parent:
+        individual["score"] = knnGPU.scoreSolution(train_data[:,individual["chromosome"]], train_labels)
+
+    parent.sort(order="score")
+
+
+    #selection by binary tournament
+    selected_parent_idx = np.empty(p_size, dtype=np.int32)
+
+    for idx in selected_parent_idx:
+        idx = np.random.randint(np.random.randint(0,p_size), p_size)
+
+    children = np.zeros(p_size, dtype=datatype)
+
+    for first_parent_idx, second_parent_idx, first_son, second_son in idx[::2], idx{1::2], children[::2], children[1::2]:
+        cross(parent[first_parent_idx], parent[second_parent_idx], first_son, second_son)
+
+    for son in children:
+        son["score"] = knnGPU.scoreSolution(train_data[:,son["chromosome"]], train_labels)
+
+    children.sort(order="score")
